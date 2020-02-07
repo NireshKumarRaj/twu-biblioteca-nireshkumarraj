b6247dd83ed17fe534767e5e7da0d3a2843e3d1d
Niresh Kumar R <nireshkumar.r@thoughtworks.com>
2020-02-07 16:50:12 +0530

    Initial Commit

---
+++ src/test/java/com/twu/biblioteca/BibliotecaTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BibliotecaTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
