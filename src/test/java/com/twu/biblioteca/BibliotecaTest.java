895e408370971376ac90c51d60d0b636cc36e4af
Niresh Kumar R <nireshkumar.r@thoughtworks.com>
2020-02-07 17:03:06 +0530

    Requirement 1: Welcome Message is displayed when start

--- src/test/java/com/twu/biblioteca/BibliotecaTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaTest.java
@@ -1,13 +1,26 @@
 package com.twu.biblioteca;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BibliotecaTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testShouldCheckIfWelcomeMessageIsPrinted() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        bibliotecaApp.start();
+
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", outContent.toString().trim());
+        System.setOut(originalOut);
     }
 }
