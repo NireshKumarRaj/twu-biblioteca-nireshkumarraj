1838f43dec9063eddf2f3ef459f7f6d0f816d0a2
Niresh Kumar R <nireshkumar.r@thoughtworks.com>
2020-02-08 12:56:26 +0530

    Requirement 4:  Menu can display an available option

---
+++ src/main/java/com/twu/biblioteca/Menu.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+
+import java.util.List;
+
+public class Menu {
+
+    private List<String> menuList;
+
+    public Menu(List<String> menuList) {
+        this.menuList = menuList;
+    }
+
+    public void display() {
+        int menuItemNumber = 1;
+        for (String menuItem : menuList) System.out.println(menuItemNumber + ". " + menuItem);
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/MenuTest.java
@@ -0,0 +1,40 @@
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    private PrintStream originalOut;
+    private ByteArrayOutputStream outContent;
+
+    @BeforeEach
+    void setUp() {
+        outContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testIfMenuIsDisplayedInCommandLine() {
+        Menu menu = new Menu(Collections.singletonList("List Books"));
+
+        menu.display();
+
+        assertEquals("1. List Books", outContent.toString().trim());
+    }
+}
