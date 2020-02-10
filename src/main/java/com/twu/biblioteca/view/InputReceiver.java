a436793df9791907eedaef72d3ff12e45e4d9e2e
= <nireshkumar.r@thoughtworks.com>
2020-02-10 22:18:18 +0530

    Add. InputReceiver to receive input from user.

---
+++ src/main/java/com/twu/biblioteca/view/InputReceiver.java
@@ -0,0 +1,28 @@
+package com.twu.biblioteca.view;
+
+import java.util.Scanner;
+
+public class InputReceiver {
+
+    private static final InputReceiver inputReceiver = new InputReceiver();
+    private Scanner scanner;
+
+    private InputReceiver(){
+    }
+
+    public static InputReceiver getInputReceiver(){
+        if (inputReceiver.scanner == null){
+            inputReceiver.scanner = new Scanner(System.in);
+        }
+        return inputReceiver;
+    }
+
+    public String readLine() {
+        return scanner.nextLine();
+    }
+
+    public void reset(){
+        scanner.close();
+        scanner = null;
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/view/InputReceiverTest.java
@@ -0,0 +1,24 @@
+package com.twu.biblioteca.view;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputReceiverTest {
+
+    @Test
+    void testShouldCheckIfUserInputIsReceived() {
+        InputStream inputStream = System.in;
+        System.setIn(new ByteArrayInputStream("Agile".getBytes()));
+        InputReceiver inputReceiver = InputReceiver.getInputReceiver();
+
+        String input = inputReceiver.readLine();
+        inputReceiver.reset();
+
+        assertEquals("Agile", input);
+        System.setIn(inputStream);
+    }
+}
