12136a61193ced505d8f6f9b108e60e895981dfe
= <nireshkumar.r@thoughtworks.com>
2020-02-12 15:40:03 +0530

    Refactor. Bibiloteca app takes in a list of models.

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -2,42 +2,36 @@ package com.twu.biblioteca;
 
 import com.twu.biblioteca.controller.BibliotecaController;
 import com.twu.biblioteca.data.DummyDataGenerator;
-import com.twu.biblioteca.model.Authenticator;
-import com.twu.biblioteca.model.Library;
 import com.twu.biblioteca.model.Menu;
-import com.twu.biblioteca.model.MovieLibrary;
+import com.twu.biblioteca.model.Model;
 import com.twu.biblioteca.view.UI;
+
+import java.util.List;
 
 public class BibliotecaApp {
 
-    private Library library;
+    private List<Model> models;
     private Menu menu;
-    private MovieLibrary movieLibrary;
-    private Authenticator authenticator;
 
-    public BibliotecaApp(Library library, Menu menu, MovieLibrary movieLibrary, Authenticator authenticator) {
-        this.library = library;
+    public BibliotecaApp(Menu menu, List<Model> models) {
+        this.models = models;
         this.menu = menu;
-        this.movieLibrary = movieLibrary;
-        this.authenticator = authenticator;
     }
 
     public static void main(String[] args) {
         DummyDataGenerator dummyDataGenerator = new DummyDataGenerator();
         new BibliotecaApp(
-                dummyDataGenerator.getLibrary(),
                 dummyDataGenerator.getMenu(),
-                dummyDataGenerator.getMovieLibrary(),
-                dummyDataGenerator.getAuthenticator()
+                List.of(dummyDataGenerator.getLibrary(),
+                        dummyDataGenerator.getMovieLibrary(),
+                        dummyDataGenerator.getAuthenticator())
         ).start();
     }
 
     public void start() {
         UI ui = new UI(System.out);
         ui.addModel(menu);
-        ui.addModel(library);
-        ui.addModel(movieLibrary);
-        ui.addModel(authenticator);
+        models.forEach(ui::addModel);
         BibliotecaController bibliotecaController = new BibliotecaController(menu, ui);
         bibliotecaController.start();
     }
