6b8d315f25f0e2df96359f904faeb87f23770063
= <nireshkumar.r@thoughtworks.com>
2020-02-12 11:43:38 +0530

    Requirement 2.3 | Refactor. Library checked out book data structure changed to map.

--- src/main/java/com/twu/biblioteca/model/Library.java
+++ src/main/java/com/twu/biblioteca/model/Library.java
@@ -2,30 +2,32 @@ package com.twu.biblioteca.model;
 
 import com.twu.biblioteca.view.View;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Library implements Model {
 
     // TODO - what can be an alternate implementation of this Libarary? Think about it and get back. Try to see if that's better - simplifies, complicates the implementation
     private List<Book> books;
-    private List<Book> checkedOutBooks;
+    private Map<Book, String> checkedOutBooks;
     private View view;
 
     public Library(List<Book> books) {
         this.books = books;
-        this.checkedOutBooks = new ArrayList<>();
+        this.checkedOutBooks = new HashMap<>();
     }
 
-    private Optional<Book> getBook(List<Book> bookList, String bookName) {
-        return bookList.stream().filter(book -> book.is(bookName)).findFirst();
+    private Optional<Book> getBook(Stream<Book> bookList, String bookName) {
+        return bookList.filter(book -> book.is(bookName)).findFirst();
     }
 
     public void listAvailableBooks() {
         this.view.display(this.books.stream()
-                .filter(book -> !this.checkedOutBooks.contains(book))
+                .filter(book -> !this.checkedOutBooks.containsKey(book))
                 .map(Book::getDetails)
                 .collect(Collectors.toList()));
     }
@@ -33,9 +35,9 @@ public class Library implements Model {
     public void checkout(String bookName) {
         final String BOOK_NOT_AVAILABLE_MESSAGE = "Sorry, that book is not available";
         final String CHECKOUT_SUCCESS_MESSAGE = "Thank you! Enjoy the book";
-        Optional<Book> book = getBook(books, bookName).filter(item -> !checkedOutBooks.contains(item));
+        Optional<Book> book = getBook(books.stream(), bookName).filter(item -> !checkedOutBooks.containsKey(item));
         if (book.isPresent()) { // TODO - see if you can get rid of the if-else. Can we be polymorphic, over what? - low priority
-            checkedOutBooks.add(book.get());
+            checkedOutBooks.put(book.get(), view.getLoggedInUser());
             notifyListener(CHECKOUT_SUCCESS_MESSAGE);
         } else {
             notifyListener(BOOK_NOT_AVAILABLE_MESSAGE);
@@ -45,7 +47,7 @@ public class Library implements Model {
     public void returnBook(String bookName) {
         String BOOK_INVALID_MESSAGE = "That is not a valid book to return.";
         String BOOK_RETURN_SUCCESS_MESSAGE = "Thank you for returning the book";
-        Optional<Book> book = getBook(checkedOutBooks, bookName);
+        Optional<Book> book = getBook(checkedOutBooks.keySet().stream(), bookName);
         if (book.isPresent()) {
             checkedOutBooks.remove(book.get());
             notifyListener(BOOK_RETURN_SUCCESS_MESSAGE);
