a71caab0dfad0e1d88364c68cdb1a71313c4eb3e
= <nireshkumar.r@thoughtworks.com>
2020-02-12 15:07:07 +0530

    Requirement 2.2 | Add. MovieCheckout can invoke movielibrary checkout.

--- src/main/java/com/twu/biblioteca/menuitem/MovieCheckOut.java
+++ src/main/java/com/twu/biblioteca/menuitem/MovieCheckOut.java
@@ -1,16 +1,20 @@
 package com.twu.biblioteca.menuitem;
 
+import com.twu.biblioteca.model.MovieLibrary;
+
 public class MovieCheckOut implements MenuItem {
 
     private final String name;
+    private MovieLibrary movieLibrary;
 
-    public MovieCheckOut() {
+    public MovieCheckOut(MovieLibrary movieLibrary) {
+        this.movieLibrary = movieLibrary;
         name = "Checkout movie";
     }
 
     @Override
     public void execute() {
-
+        movieLibrary.checkout("The Social Network");
     }
 
     @Override
--- src/test/java/com/twu/biblioteca/menuitem/MovieCheckOutTest.java
+++ src/test/java/com/twu/biblioteca/menuitem/MovieCheckOutTest.java
@@ -1,16 +1,28 @@
 package com.twu.biblioteca.menuitem;
 
+import com.twu.biblioteca.model.MovieLibrary;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class MovieCheckOutTest {
     @Test
     void testShouldCheckIfMenuOptionIsReturned() {
-        MovieCheckOut movieCheckOut = new MovieCheckOut();
+        MovieCheckOut movieCheckOut = new MovieCheckOut(mock(MovieLibrary.class));
 
         String name = movieCheckOut.getName();
 
         assertEquals("Checkout movie", name);
+    }
+
+    @Test
+    void testShouldCheckIfMovieCheckOutOptionIsAdded() {
+        MovieLibrary movieLibrary = mock(MovieLibrary.class);
+        MovieCheckOut movieCheckOut = new MovieCheckOut(movieLibrary);
+
+        movieCheckOut.execute();
+
+        verify(movieLibrary, times(1)).checkout("The Social Network");
     }
 }
