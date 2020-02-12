d41e5bca71c66bc414722796775fc39e9193af33
= <nireshkumar.r@thoughtworks.com>
2020-02-12 14:53:01 +0530

    Requirement 2.3 | Add. CheckOutBooks Menu item can invoke library method.

--- src/main/java/com/twu/biblioteca/menuitem/CheckedOutBooks.java
+++ src/main/java/com/twu/biblioteca/menuitem/CheckedOutBooks.java
@@ -1,16 +1,20 @@
 package com.twu.biblioteca.menuitem;
 
+import com.twu.biblioteca.model.Library;
+
 public class CheckedOutBooks implements MenuItem {
 
     private final String name;
+    private Library library;
 
-    public CheckedOutBooks() {
+    public CheckedOutBooks(Library library) {
+        this.library = library;
         this.name = "List checked out books";
     }
 
     @Override
     public void execute() {
-
+        library.listCheckOutBooks();
     }
 
     @Override
--- src/test/java/com/twu/biblioteca/menuitem/CheckedOutBooksTest.java
+++ src/test/java/com/twu/biblioteca/menuitem/CheckedOutBooksTest.java
@@ -1,17 +1,29 @@
 package com.twu.biblioteca.menuitem;
 
+import com.twu.biblioteca.model.Library;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class CheckedOutBooksTest {
 
     @Test
     void testShouldCheckIfNameIsAvailable() {
-        CheckedOutBooks checkedOutBooksMenuItem = new CheckedOutBooks();
+        CheckedOutBooks checkedOutBooksMenuItem = new CheckedOutBooks(mock(Library.class));
 
         String menuItemName = checkedOutBooksMenuItem.getName();
 
         assertEquals("List checked out books", menuItemName);
+    }
+
+    @Test
+    void testShouldCheckIfCheckedOutBooksAreDisplayed() {
+        Library library = mock(Library.class);
+        CheckedOutBooks checkedOutBooksMenuItem = new CheckedOutBooks(library);
+
+        checkedOutBooksMenuItem.execute();
+
+        verify(library, times(1)).listCheckOutBooks();
     }
 }
