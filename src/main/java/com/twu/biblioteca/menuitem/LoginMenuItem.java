57856325b103b5c72aabab1b7e000ab108a27c68
= <nireshkumar.r@thoughtworks.com>
2020-02-12 10:38:46 +0530

    Requirement 2.3 | Add. LoginMenuItem can authenticate user.

--- src/main/java/com/twu/biblioteca/menuitem/LoginMenuItem.java
+++ src/main/java/com/twu/biblioteca/menuitem/LoginMenuItem.java
@@ -1,16 +1,20 @@
 package com.twu.biblioteca.menuitem;
 
+import com.twu.biblioteca.Authenticator;
+
 public class LoginMenuItem implements MenuItem {
 
     private final String name;
+    private Authenticator authenticator;
 
-    public LoginMenuItem() {
+    public LoginMenuItem(Authenticator authenticator) {
+        this.authenticator = authenticator;
         name = "Login";
     }
 
     @Override
     public void execute() {
-
+        authenticator.authenticate("123-4567", "test1");
     }
 
     @Override
--- src/test/java/com/twu/biblioteca/menuitem/LoginMenuItemTest.java
+++ src/test/java/com/twu/biblioteca/menuitem/LoginMenuItemTest.java
@@ -1,16 +1,28 @@
 package com.twu.biblioteca.menuitem;
 
+import com.twu.biblioteca.Authenticator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class LoginMenuItemTest {
     @Test
     void testShouldBeAbleToReturnMenuItemName() {
-        LoginMenuItem loginMenuItem = new LoginMenuItem();
+        LoginMenuItem loginMenuItem = new LoginMenuItem(mock(Authenticator.class));
 
         String name = loginMenuItem.getName();
 
         assertEquals("Login", name);
+    }
+
+    @Test
+    void testShouldBeAbleToAuthenticateUser() {
+        Authenticator authenticator = mock(Authenticator.class);
+        LoginMenuItem loginMenuItem = new LoginMenuItem(authenticator);
+
+        loginMenuItem.execute();
+
+        verify(authenticator, times(1)).authenticate("123-4567", "test1");
     }
 }
